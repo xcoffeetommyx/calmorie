3fc1870416fba977b013179b546403f35abaf9fa
xcoffeetommyx <xcoffeetommyx@gmail.com>
2026-03-21 21:03:14 -0500

    Add files via upload

--- src/components/layout/AppLogo.tsx
+++ src/components/layout/AppLogo.tsx
@@ -17,6 +17,7 @@
 
 import Image from 'next/image'
 import { cn } from '@/lib/utils/cn'
+import logoSrc from '../../../public/icons/calmorie-app-icon.png'
 
 interface AppLogoProps {
   /** Width and height in pixels. Default 24. */
@@ -28,7 +29,7 @@ interface AppLogoProps {
 export function AppLogo({ size = 24, className }: AppLogoProps) {
   return (
     <Image
-      src="/icons/calmorie-app-icon.png"
+      src={logoSrc}
       alt="Calmorie icon"
       width={size}
       height={size}
